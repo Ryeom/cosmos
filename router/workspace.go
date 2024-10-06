ed008eeacac5a4beb7ea1b852119aa8d77d373ef
ryeom <dmfua2@gmail.com>
2024-10-07 00:49:17 +0900

    commit

--- router/workspace.go
+++ router/workspace.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"fmt"
+	"github.com/Ryeom/cosmos/mongo"
 	"github.com/Ryeom/cosmos/service/workspace"
 	"github.com/labstack/echo/v4"
 	"net/http"
@@ -14,7 +15,9 @@ func CreateWorkspace(c echo.Context) error {
 		return c.JSON(http.StatusBadRequest, result)
 	}
 	w.PrintDataInfo()
-	err := w.Save()
+	//TODO validate, permission (level with creatable workspace count check)
+
+	err := w.Insert()
 	if err != nil {
 		return c.JSON(http.StatusInternalServerError, result)
 	}
@@ -23,12 +26,16 @@ func CreateWorkspace(c echo.Context) error {
 
 func UpdateWorkspace(c echo.Context) error {
 	result := GetDefaultResult()
-	w := workspace.NewWorkspace()
+	w := workspace.Workspace{}
 	if bindErr := c.Bind(&w); bindErr != nil {
 		return c.JSON(http.StatusBadRequest, result)
 	}
-	//
-	fmt.Println(w)
+	//TODO validate, permission (level with creatable workspace count check)
+
+	err := w.Update()
+	if err != nil {
+		return c.JSON(http.StatusInternalServerError, result)
+	}
 	return c.JSON(http.StatusOK, result.OK())
 }
 
@@ -45,11 +52,14 @@ func DeleteWorkspace(c echo.Context) error {
 
 func GetWorkspace(c echo.Context) error {
 	result := GetDefaultResult()
-	w := workspace.NewWorkspace()
+	w := struct {
+		UserId string `json:"user_id"`
+		Id     string `json:"id"`
+	}{}
 	if bindErr := c.Bind(&w); bindErr != nil {
 		return c.JSON(http.StatusBadRequest, result)
 	}
-
+	mongo.SelectAll("workspace", map[string]string{})
 	fmt.Println(w)
 	return c.JSON(http.StatusOK, result.OK())
 }
--- service/workspace/workspace.go
+++ service/workspace/workspace.go
@@ -1,27 +1,31 @@
 package workspace
 
 import (
+	"errors"
 	"fmt"
-	"github.com/Ryeom/cosmos/mongo"
-	"github.com/Ryeom/cosmos/util"
-	//"go.mongodb.org/mongo-driver/v2/bson"
+	"github.com/Ryeom/cosmos/log"
+	"github.com/Ryeom/cosmos/psql"
+	"gorm.io/gorm/clause"
+	"time"
+
 	"reflect"
 )
 
 type Workspace struct {
-	Index            int64    `json:"index,omitempty" bson:"index"`
-	Id               string   `json:"id" bson:"id"`
-	Name             string   `json:"name" bson:"name"`
-	BusinessCategory string   `json:"businessCategory" bson:"businessCategory"` // 업종
-	Address          string   `json:"address" bson:"address"`                   // 주소
-	Grade            string   `json:"grade" bson:"grade"`                       // 등급
-	InService        []string `json:"inService" bson:"inService"`               // 사용중인 서비스
+	Sno              uint   `json:"sno" gorm:"-:all;primary_key"`
+	Name             string `json:"name" gorm:"column:name"`
+	BusinessCategory string `json:"business_category" gorm:"column:business_category"` // 업종
+	Address          string `json:"address" gorm:"column:address"`                     // 주소
+	Grade            string `json:"grade" gorm:"column:grade"`                         // 등급
+	OwnerId          string `json:"ownerId" gorm:"column:owner_id"`
+
+	CreatedAt time.Time
+	UpdatedAt time.Time
+	DeletedAt *time.Time `sql:"index"`
 }
 
 func NewWorkspace() Workspace {
-	w := Workspace{
-		Id: util.GetUUID(),
-	}
+	w := Workspace{}
 	return w
 }
 
@@ -33,15 +37,56 @@ func (w Workspace) PrintDataInfo() {
 		t := e.Type().Field(i)
 		fmt.Printf("[Name: %s] Type: %s | Value: %v\n",
 			t.Name, t.Type, v.Interface())
-
 	}
 }
 
-func (w Workspace) Save() error {
-	var err error
-	d := mongo.ToBsonD(w)
-	err = mongo.InsertOne("workspace", d)
-	return err
+func (w Workspace) Insert() error {
+	db := psql.Postgresql.GetDB().
+		Table(`cosmos.t_workspace`).
+		Clauses(
+			clause.Returning{
+				Columns: []clause.Column{
+					{Name: "sno"},
+					{Name: "name"},
+				},
+			},
+		).Create(&w)
+	if db.Error != nil {
+		log.Logger.Error(db.Error.Error())
+		return db.Error
+	}
+	return nil
+}
+func (w Workspace) Update() error {
+	db := psql.Postgresql.GetDB().
+		Table(`cosmos.t_workspace`).
+		Clauses(
+			clause.Returning{
+				Columns: []clause.Column{
+					{Name: "sno"},
+					{Name: "name"},
+				},
+			},
+		).
+		Where("sno = ?", w.Sno).
+		UpdateColumns(&w)
+
+	if db.Error != nil {
+		log.Logger.Error(db.Error.Error())
+
+		return db.Error
+	}
+	return nil
+}
+
+func (w Workspace) Delete() error {
+	return errors.New("")
+}
+
+func Select() Workspace {
+	w := Workspace{}
+
+	return w
 }
 
 func MaxIndex() int64 {
